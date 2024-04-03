1b1f9c5d44b47a487a9d855385ee6613b0b40ea2
Kevin Uruchi <uruchicoarite@gmail.com>
2024-04-03 10:48:18 -0400

    communication with payment-ms

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,10 +2,11 @@ import { HttpStatus, Inject, Injectable, Logger, OnModuleInit } from '@nestjs/co
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { OrderPaginationDto } from './dto';
+import { OrderPaginationDto, PaidOrderDto } from './dto';
 import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
 import { NATS_SERVICE } from 'src/config';
 import { firstValueFrom } from 'rxjs';
+import { OrderWithProducts } from './interfaces/order-with-products.interface';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -158,6 +159,50 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
       where: { id },
       data: { status: status }
     });
+  }
+
+  async createPaymentSession( order: OrderWithProducts ) {
+
+    const paymentSession = await firstValueFrom(
+      this.client.send('create.payment.session',{
+        orderId: order.id,
+        currency: 'usd',
+        items: order.OrderItem.map( item => ({
+          name: item.name,
+          price: item.price,
+          quantity: item.quantity,
+        }))
+      })
+    )
+
+    return paymentSession;
 
   }
+
+  async paidOrder( paidOrderDto: PaidOrderDto ) {
+
+    this.logger.log('Order Paid');
+    this.logger.log(paidOrderDto);
+
+    const order = await this.order.update({
+      where: { id: paidOrderDto.orderId },
+      data: {
+        status: 'PAID',
+        paid: true,
+        paidAt: new Date(),
+        stripeChargeId: paidOrderDto.stripePaymentId,
+
+        // Relación
+        OrderReceipt: {
+          create: {
+            receiptUrl: paidOrderDto.receiptUrl
+          }
+        }
+      }
+    });
+
+    return order;
+
+  }
+
 }
